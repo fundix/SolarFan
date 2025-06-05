f2d26a97730c04625e86f15f04f190bbdac20c20
Karel Funda <karel.funda@gmail.com>
2025-06-05 14:27:03 +0200

    ignore credentials

--- src/credentials.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-const char apn[] = "hologram";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-const char *broker = "";   // MQTT broker
-const int mqttPort = 1883; // MQTT port
-const char *MQTT_USER = "";
-const char *MQTT_PASS = ""; // MQTT password
